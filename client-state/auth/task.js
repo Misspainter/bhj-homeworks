f8029fb65b7c6903a7cc8114c3deb80ae143269b
Misspainter <117083864+Misspainter@users.noreply.github.com>
2023-08-08 15:19:00 +0300

    second commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,39 +1,24 @@
-const control = document.querySelectorAll('.control');
-const form = document.getElementById('signin__form');
-const welcomeUser = document.querySelector('.welcome');
-const xhr = new XMLHttpRequest();
-const activeFlag = true;
-
-function sendData(payload) {
-  xhr.open("POST", 'https://students.netoservices.ru/nestjs-backend/auth')
-  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  xhr.responseType = 'json'
-  xhr.send(JSON.stringify(payload));
-
-  xhr.onload = function () {
-    welcomeUser.classList.add('welcome_active')
-    let resposeObj = xhr.response;
-    if (resposeObj.success) {
-      document.getElementById('user_id').outerText = resposeObj.user_id;
-      document.getElementById('signin').className = 'signin'
-      localStorage.setItem('user', String(resposeObj.user_id))
-    } else {
-      document.getElementById('signin').classList.add('signin_active')
-    }
-  }
+let xhr = new XMLHttpRequest()
+xhr.open("POST","https://students.netoservices.ru/nestjs-backend/auth")
+let form = document.getElementById('signin__form')
+if(localStorage.getItem("id")){
+    document.querySelector('.signin_active').classList.remove('signin_active')
+    document.querySelector('.welcome').classList.add('welcome_active')
+    document.getElementById('user_id').textContent = localStorage.getItem("id");
 }
-
-function check() {
-
-}
-
-function formSend(event) {
-  event.preventDefault()
-  let info;
-  control.forEach((el) => {
-    info = { ...info, [el.name]: el.value }
-  })
-  sendData(info)
-}
-
-form.addEventListener('submit', formSend)
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const f = new FormData(form)
+    xhr.addEventListener('load',()=>{
+            let parse = JSON.parse(xhr.response)
+            if(parse.success){
+                document.querySelector('.signin_active').classList.remove('signin_active')
+                document.querySelector('.welcome').classList.add('welcome_active')
+                localStorage.setItem('id',parse.user_id)
+                document.getElementById('user_id').textContent = localStorage.getItem("id");
+            }else{
+                alert('Неверный логин/пароль')
+            }
+    })
+    xhr.send(f)
+})
